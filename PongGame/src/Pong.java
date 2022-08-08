05f2a2be10403b155d909d5f3740f37b6af44ec5
Polrek <rhys.n.edlin@gmail.com>
2022-08-08 20:32:18 +1200

    added pong panel class

--- PongGame/src/Pong.java
+++ PongGame/src/Pong.java
@@ -1,5 +1,5 @@
 import javax.swing.JFrame;
-public class Pong extends JFrame {
+public class Pong extends JFrame { //the window which the game runs in
 	
 	private final static String WINDOW_TITLE = "Pong";
 	private final static int WINDOW_WIDTH = 800;
@@ -10,12 +10,18 @@ public class Pong extends JFrame {
     	setTitle(WINDOW_TITLE);
     	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     	setResizable(false);
+    	add(new PongPanel());
     	setVisible(true);
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 	
 	public static void main(String[] args) {
-		new Pong();
-	}
+		//this is to make sure the GUI gets created within the Event Dispatch thread because the swing GUI components are not threadsafe
+		javax.swing.SwingUtilities.invokeLater(new Runnable() { //delays the GUI creation task until the initial thread's task have been completed.
+			public void run() {
+				new Pong();
+			}
+		});
+		}
 
 }
