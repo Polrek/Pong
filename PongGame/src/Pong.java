13b37ad98a51d670aaa9864671809d69266ba088
Polrek <rhys.n.edlin@gmail.com>
2022-08-08 20:00:21 +1200

    added static variables for window title, height and width, and updated Pong method to use these values

--- PongGame/src/Pong.java
+++ PongGame/src/Pong.java
@@ -1,9 +1,21 @@
+import javax.swing.JFrame;
+public class Pong extends JFrame {
+	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
 
-public class Pong {
-
+	//this is the constructor
+	public Pong() {
+    	setTitle(WINDOW_TITLE);
+    	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    	setResizable(false);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+	
 	public static void main(String[] args) {
-		// check if I can commit this to github
-
+		new Pong();
 	}
 
 }
